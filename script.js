7d9b5dadf9292cb360f77248ea2f740956878766
Deydre <deydrealonso@gmail.com>
2024-09-28 16:05:02 +0200

    improved/HTML and CSS tags + added/form fields + style/form new flexbox columns

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-
-// Al hacer click en el menú, cambiar un icono por otro
-
-let openMenu = document.querySelector("#openMenu");
-let closeMenu = document.querySelector("#closeMenu");
-
-// Almacenamos la imagen para volver a ella más tarde
-let homeImg = document.querySelector("a[href='index.html']").innerHTML;
-// Almacenamos el contenedor para cambiar más tarde lo de dentro 
-let homeContainer = document.querySelector("a[href='index.html']");
+document.addEventListener('DOMContentLoaded', function() {
 
 
-openMenu.addEventListener('click', function() {
-    // Cambiar la clase para ocultarlo y sacar la otra
-    openMenu.setAttribute("class", "hidden");
-    closeMenu.setAttribute("class", "");
-    // Cambiar la casita por texto "Home"
-    homeContainer.innerHTML = "Home";
-});
+        // CLICK EN MENÚ = Cambiar icono por otro + Cambiar img Home por texto "Home"
 
-closeMenu.addEventListener('click', function() {
-    // Cambiar la clase para ocultarlo y sacar la otra
-    closeMenu.setAttribute("class", "hidden");
-    openMenu.setAttribute("class", "");
-    // Cambiar la casita por la imagen almacenada
-    homeContainer.innerHTML = homeImg;
+    // Almacenar la ruta de los 2 iconos en variables
+    let openMenu = document.querySelector("#openMenu");
+    let closeMenu = document.querySelector("#closeMenu");
+
+    // Cambiar img HOME por "Home"
+    // Almacenamos la imagen de HOME para volver a ella más tarde
+    let homeImg = document.querySelector("a[href='index.html']").innerHTML;
+    // Almacenamos el contenedor para cambiar más tarde lo de dentro 
+    let homeContainer = document.querySelector("a[href='index.html']");
+
+    openMenu.addEventListener('click', function() {
+        // Cambiar la clase para ocultarlo y sacar la otra
+        openMenu.setAttribute("class", "hidden");
+        closeMenu.setAttribute("class", "");
+        // Cambiar la casita por texto "Home"
+        homeContainer.innerHTML = "Home";
+    });
+
+    closeMenu.addEventListener('click', function() {
+        // Cambiar la clase para ocultarlo y sacar la otra
+        closeMenu.setAttribute("class", "hidden");
+        openMenu.setAttribute("class", "");
+        // Cambiar la casita por la imagen almacenada
+        homeContainer.innerHTML = homeImg;
+    });
+    
+    // VALIDACIÓN DEL FORMULARIO
+    let form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // PARALIZA EL ENVÍO DEL FORMULARIO
+
+        console.log('elementos', event.target.elements)
+        console.log(event.target.elements) // = En elements están todos lo inputs del formulario
+    	// event.target.favorite.value // = Del input que tiene el id "favorite" sacamos 
+    	// // el valor (por ejemplo si está vacío pedirlo de nuevo))
+    	// event.target.email.value
+
+    	// Si no hay errores...
+    	// e.target.submit(); // ENVÍA EL FORMULARIO
+    	// Pintar confirmación de que está enviado con éxito
+    });
+
+
+
 });
